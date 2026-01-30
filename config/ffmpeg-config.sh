4e588d4402a8c58dcc494c51b55f4cb44a5fbffa
kyjcoope <51489123+kyjcoope@users.noreply.github.com>
2026-01-30 14:06:37 -0500

    Update ffmpeg-config.sh

--- config/ffmpeg-config.sh
+++ config/ffmpeg-config.sh
@@ -2,8 +2,8 @@
 # FFmpeg shared configuration flags
 # Edit this file to customize the build
 
-# FFmpeg version (git tag) - 4.4.5 LTS (latest 4.4.x with SDK fixes)
-FFMPEG_VERSION="${FFMPEG_VERSION:-n4.4.5}"
+# FFmpeg version (git tag) - 6.0 (libavutil 58.2)
+FFMPEG_VERSION="${FFMPEG_VERSION:-n6.0}"
 
 # Libraries to build
 FFMPEG_LIBS=(
